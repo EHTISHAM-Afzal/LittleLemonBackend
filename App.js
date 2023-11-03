744eadeffb67dce5691db53ea4882f784833e866
EHTISHAM-Afzal <khaj3172@gmail.com>
2023-11-03 10:36:25 +0500

    first commit Changes to be committed: 	new file:   .gitignore 	new file:   App.js 	new file:   Models/CatagoryModel.js 	new file:   Models/DishModel.js 	new file:   MongoDB/Connect.js 	new file:   Routes/Dish.js 	new file:   Routes/catagories.js 	new file:   package.json 	new file:   yarn.lock

---
+++ App.js
@@ -0,0 +1,33 @@
+import express from "express";
+import morgan from 'morgan';
+import DishRoutes from './Routes/Dish.js'
+import catagoriesRouter from './Routes/catagories.js';
+import connectDB from './MongoDB/Connect.js';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const app = express();
+app.use(express.json({ limit: "25mb" }));
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/api/dishes", DishRoutes);
+app.use("/api/catagory", catagoriesRouter);
+
+
+const port = process.env.PORT || 3002;
+
+const StartServer = async () => {
+    try {
+        // eslint-disable-next-line no-undef
+        connectDB(process.env.MONGODB_URL);
+        app.listen(port, () => {
+            console.log(`app is runnig on ${port} `);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+StartServer();
